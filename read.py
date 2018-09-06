34c748932a1f599d6017335310b633a5a2c391b2
Sam Chen <free6323@hotmail.com>
2018-09-06 22:41:50 +0800

    add avg part

--- read.py
+++ read.py
@@ -2,6 +2,15 @@ message = []
 with open('reviews.txt', 'r') as files:
 	for file in files:
 		message.append(file)
-		print('進度: ', len(message), '/', 1000000)
+		if len(message) % 1000 == 0:
+			print('進度: ', len(message), '/', 1000000)
+print('總長度: ', len(message), '筆資料')
 print(message[0])
+avg = 0
+i = 0
+while i < 1000000:
+	avg = len(message[i]) + avg
+	i += 1
+avg = int(avg / i)
+print('平均長度: ', avg)
 
