dff852a1c418c7a8fb26d1e300484b3d57b259ea
Sam Chen <free6323@hotmail.com>
2018-09-06 22:48:36 +0800

    the avg part change to for loop

--- read.py
+++ read.py
@@ -7,10 +7,8 @@ with open('reviews.txt', 'r') as files:
 print('總長度: ', len(message), '筆資料')
 print(message[0])
 avg = 0
-i = 0
-while i < 1000000:
-	avg = len(message[i]) + avg
-	i += 1
-avg = int(avg / i)
+for len_avg in message:
+	avg = avg + len(len_avg)
+avg = int(avg / len(message))
 print('平均長度: ', avg)
 
