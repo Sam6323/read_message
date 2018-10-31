e419528b26d414ec2894c14d40277ed29423a98f
Sam Chen <free6323@hotmail.com>
2018-10-31 22:29:03 +0800

    word count

--- read.py
+++ read.py
@@ -5,6 +5,8 @@ with open('reviews.txt', 'r') as files:
 		if len(message) % 1000 == 0:
 			print('進度: ', len(message), '/', 1000000)
 print('總長度: ', len(message), '筆資料')
+# print(message[0])
+
 print(message[0])
 avg = 0
 for len_avg in message:
@@ -16,5 +18,34 @@ print('平均長度: ', avg)
 new = []
 for line in message:
 	if len(line) < 100:
-		new.append(line)
+		new.append(line)	
 print('總共有: ', len(new), '行小於100個字')
+
+# 文字計數
+wc = {} # wc = word_count
+for line in message:
+	data = line.split() # split預設為空白,不輸入會使用預設值
+	
+	for word in data:
+		if word in wc:
+			wc[word] += 1
+			
+		else:
+			wc[word] = 1 # 新增key
+			
+for word in wc: # 印出出現超過1000000次的字
+	if wc[word] > 1000000:
+		print(word, wc[word])
+
+while True: # 輸入要查詢的字
+	key = input('請輸入要查詢的字: ')
+	if key == 'q':
+		break
+	if key in wc:
+		print(key, '共出現: ', wc[key])
+	else:
+		print('查詢不到這個字 !!!')
+
+
+
+
